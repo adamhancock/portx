3e7600a225fec29fb05892dff0fc04106ae11ed4
adammhancock@gmail.com <adammhancock@gmail.com>
2020-03-10 08:08:26 +0000

    environment variables

---
+++ envArray.js
@@ -0,0 +1,5 @@
+module.exports = function (envValue) {
+  return {
+    env: envValue
+  }
+}
--- index.js
+++ index.js
@@ -1,46 +1,53 @@
-const chalk = require("chalk");
-const fs = require("fs");
-const path = require("path");
-const dnsResolves = require("./dnsResolves");
-const hostCheck = require("./hostCheck");
-const isIp = require("is-ip");
+const chalk = require('chalk')
+
+const dnsResolves = require('./dnsResolves')
+const hostCheck = require('./hostCheck')
+const isIp = require('is-ip')
+
+const fileTemplating = require('./fileTemplating')
+
+
 
 if (process.argv[2] == undefined) {
   console.log(
     chalk.red(
-      "Please pass a JSON file path as a parameter. For example porttest.exe hosts.json"
+      'Please pass a JSON file path as a parameter. For example porttest.exe hosts.json'
     )
-  );
-  process.exit();
+  )
+  process.exit()
 }
 
-const hosts = JSON.parse(
-  fs.readFileSync(path.join(process.cwd(), process.argv[2]))
-);
+const hosts = fileTemplating(process.argv)
+
 
 hosts.forEach(async host => {
+  if (host.env) {
+    var env = `${host.env.toUpperCase() } -`
+  } else {
+    var env = ""
+  }
   if (!isIp(host.host)) {
-    const ipaddresses = await dnsResolves(host.host);
+    const ipaddresses = await dnsResolves(host.host)
     if (ipaddresses.length == 0) {
-      console.log(chalk.red(`${host.host} does not resolve`));
+      console.log(chalk.red(`${host.host} does not resolve`))
     } else {
       ipaddresses.forEach(async address => {
         console.log(
           await hostCheck({
             host: address,
-            name: `${host.name} - ${host.host}`,
+            name: `${env} ${host.name} - ${host.host}`,
             port: host.port
           })
-        );
-      });
+        )
+      })
     }
   } else {
     console.log(
       await hostCheck({
         host: host.host,
-        name: `${host.name} - ${host.host}`,
+        name: `${env} ${host.name} - ${host.host}`,
         port: host.port
       })
-    );
+    )
   }
-});
+})
