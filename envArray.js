68a8e2e743f10612199ae063458f3d6887635102
adammhancock@gmail.com <adammhancock@gmail.com>
2020-03-29 15:35:08 +0100

    bumb minimist

--- envArray.js
+++
@@ -1,5 +0,0 @@
-module.exports = function (envValue) {
-  return {
-    env: envValue
-  }
-}
--- test.js
+++
@@ -1,3 +0,0 @@
-const Handlebars = require('handlebars')
-const template = Handlebars.compile('Name: {{name}}')
-console.log(template({ name: 'Nils' }))
