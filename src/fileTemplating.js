fcac7b4276d0ca7fa9e1468229837a8d55ddb91e
adammhancock@gmail.com <adammhancock@gmail.com>
2020-04-11 10:33:57 +0100

    added no port detection

--- src/fileTemplating.js
+++ src/fileTemplating.js
@@ -1,57 +1,62 @@
 // environment pass in for templating.
-const Handlebars = require("handlebars");
-const program = require("commander");
-const fs = require("fs");
-const path = require("path");
-const chalk = require("chalk");
+const Handlebars = require('handlebars')
+const program = require('commander')
+const fs = require('fs')
+const path = require('path')
+const chalk = require('chalk')
 
 program
-  .option("-e, --env <String>", "environment templating")
-  .option("-f, --file <String>", "file based")
-  .option("-h, --host <String>", "host based");
-program.parse(process.argv);
+  .option('-e, --env <String>', 'environment templating')
+  .option('-f, --file <String>', 'file based')
+  .option('-h, --host <String>', 'host based')
+program.parse(process.argv)
 
-module.exports = function() {
-  if (program.file == undefined && process.argv["2"] == undefined) {
-    console.log(chalk.red("No hosts specified."));
-    process.exit();
+module.exports = function () {
+  if (program.file == undefined && process.argv['2'] == undefined) {
+    console.log(chalk.red('No hosts specified.'))
+    process.exit()
   }
 
   if (program.env) {
-    const environments = program.env.split(",");
-    const hosts = [];
-    environments.forEach(environment => {
+    const environments = program.env.split(',')
+    const hosts = []
+    environments.forEach((environment) => {
       const template = Handlebars.compile(
         fs.readFileSync(path.join(process.cwd(), program.file)).toString()
-      );
+      )
       const templateResults = JSON.parse(
         template({
-          env: environment
+          env: environment,
         })
-      );
+      )
       hosts.push(
-        ...templateResults.map(result => {
+        ...templateResults.map((result) => {
           return {
             ...result,
-            env: environment
-          };
+            env: environment,
+          }
         })
-      );
-    });
-    return hosts;
+      )
+    })
+    return hosts
   } else {
     // command line hosts
     if (program.host) {
+      const host = program.host.split(':')[1]
+      const port = program.host.split(':')[1]
+      if (port == undefined) {
+        console.log(chalk.red('No port specified.'))
+        process.exit()
+      }
+
       return [
         {
-          host: program.host.split(":")[0],
-          port: program.host.split(":")[1]
-        }
-      ];
+          host: host,
+          port: port,
+        },
+      ]
     } else {
-      return JSON.parse(
-        fs.readFileSync(path.join(process.cwd(), program.file))
-      );
+      return JSON.parse(fs.readFileSync(path.join(process.cwd(), program.file)))
     }
   }
-};
+}
--- src/index.js
+++ src/index.js
@@ -1,50 +1,51 @@
 #!/usr/bin/env node
 
-const chalk = require("chalk");
+const chalk = require('chalk')
 
-const dnsResolves = require("./dnsResolves");
-const hostCheck = require("./hostCheck");
-const isIp = require("is-ip");
+const dnsResolves = require('./dnsResolves')
+const hostCheck = require('./hostCheck')
+const isIp = require('is-ip')
 
-const fileTemplating = require("./fileTemplating");
+const fileTemplating = require('./fileTemplating')
 
-const hosts = fileTemplating(process.argv);
+const hosts = fileTemplating(process.argv)
 
-hosts.forEach(async host => {
+hosts.forEach(async (host) => {
+  x
   if (host.env) {
-    var env = `${host.env.toUpperCase()} -`;
+    var env = `${host.env.toUpperCase()} -`
   } else {
-    var env = "";
+    var env = ''
   }
   if (host.name) {
-    host.name = " " + host.name + " - ";
+    host.name = ' ' + host.name + ' - '
   } else {
-    host.name = "";
+    host.name = ''
   }
   if (!isIp(host.host)) {
-    const ipaddresses = await dnsResolves(host.host);
+    const ipaddresses = await dnsResolves(host.host)
     if (ipaddresses.length == 0) {
       console.log(
         chalk.red(`* FAIL - ${env}${host.name}${host.host} does not resolve`)
-      );
+      )
     } else {
-      ipaddresses.forEach(async address => {
+      ipaddresses.forEach(async (address) => {
         console.log(
           await hostCheck({
             host: address,
             name: `${env}${host.name}${host.host}`,
-            port: host.port
+            port: host.port,
           })
-        );
-      });
+        )
+      })
     }
   } else {
     console.log(
       await hostCheck({
         host: host.host,
         name: `${env} ${host.name}${host.host}`,
-        port: host.port
+        port: host.port,
       })
-    );
+    )
   }
-});
+})
